8180f5711fecfe0416cb561313223bfedbaa31bb
seohoon-dev <sh931123@gmail.com>
2021-03-30 09:03:59 +0900

    코드 정리

--- src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -7,15 +7,6 @@ import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
-
-
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(500); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-    }
-
     @MessageMapping("/1")
     @SendTo("/topic/1")
     public Greeting first(HelloMessage message) throws Exception {
