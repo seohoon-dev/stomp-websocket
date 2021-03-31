034c502510c445638b476132e1c8b037fea7bf1c
seohoon-dev <sh931123@gmail.com>
2021-03-31 17:18:29 +0900

    코드 정리

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  outputDir: path.resolve(__dirname, "../src/main/resources/static"),
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:10000",
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+
 }
